d3140f04b30cb65ea249b3574ec4b367304084ea
iBLAK0506 <ezikpejohn098@gmail.com>
2025-08-09 18:12:52 +0100

    install react.js and tailwind css and create the first template

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// craete a function and test tailwindcss
+
+function app() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-500">
+      hello, welcome to coding
+    </h1>
+  );
+}
+
+export default app;
