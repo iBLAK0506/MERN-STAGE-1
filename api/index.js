ab5f87e92f0016118dbd4038b52fe51557c4fdbc
iBLAK0506 <ezikpejohn098@gmail.com>
2025-08-10 00:56:43 +0100

    STAGE 5 - connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
+import { error } from "console";
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGO_STRING_URL)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
